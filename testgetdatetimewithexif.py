0ddd97f512356ad0eefda35e1cef51bb99806ec0
Lauren <lauren@transmogrifier.(none)>
2013-04-12 09:51:08 -0700

    updated output exif data

--- testgetdatetimewithexif.py
+++ testgetdatetimewithexif.py
@@ -62,53 +62,11 @@ def get_exif_data(image):
 def _get_if_exist(data, key):
     print "get if exist function"
     if key in data:
-        return data[key]
+        print data[key]
         
     return None
     
-def _convert_to_degress(value):
-    print "convert to degrees function"
-    """Helper function to convert the GPS coordinates stored in the EXIF to degress in float format"""
-    d0 = value[0][0]
-    d1 = value[0][1]
-    d = float(d0) / float(d1)
- 
-    m0 = value[1][0]
-    m1 = value[1][1]
-    m = float(m0) / float(m1)
- 
-    s0 = value[2][0]
-    s1 = value[2][1]
-    s = float(s0) / float(s1)
- 
-    return d + (m / 60.0) + (s / 3600.0)
- 
-def get_lat_lon(exif_data):
-    """Returns the latitude and longitude, if available, from the provided exif_data (obtained through get_exif_data above)"""
-    print "get_lat_lon function"
-    lat = None
-    lon = None
- 
-    if "GPSInfo" in exif_data:  
-        print "GPSInfo If"    
-        gps_info = exif_data["GPSInfo"]
- 
-        gps_latitude = _get_if_exist(gps_info, "GPSLatitude")
-        gps_latitude_ref = _get_if_exist(gps_info, 'GPSLatitudeRef')
-        gps_longitude = _get_if_exist(gps_info, 'GPSLongitude')
-        gps_longitude_ref = _get_if_exist(gps_info, 'GPSLongitudeRef')
- 
-        if gps_latitude and gps_latitude_ref and gps_longitude and gps_longitude_ref:
-            print "gps_latitude if"
-            lat = _convert_to_degress(gps_latitude)
-            if gps_latitude_ref != "N":                     
-                lat = 0 - lat
- 
-            lon = _convert_to_degress(gps_longitude)
-            if gps_longitude_ref != "E":
-                lon = 0 - lon
- 
-    return lat, lon
+
  
 
 @app.route('/upload', methods=['GET', 'POST'])
@@ -126,7 +84,7 @@ def upload_file():
 
             image = Image.open(file_path)
             exif_data = get_exif_data(image)
-            print get_lat_lon(exif_data)
+            print exif_data
 
 
             # get_exif_data(file_path)
