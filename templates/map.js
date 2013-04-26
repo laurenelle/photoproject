12926ae6f7af301503d9139d769785b7edcb4c67
Lauren <lauren@transmogrifier.local>
2013-04-25 17:33:12 -0700

    added styling to map

--- templates/map.js
+++ templates/map.js
@@ -1,48 +1,38 @@
+    <script type="text/javascript">
 
 
 
-var map;
+      function makeInfoWindowEvent(map, infowindow, marker) {  
+        return function() {  
+          infowindow.open(map, marker);
+        };  
+      } 
 
-function initialize() {
-  var mapOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(-34.397, 150.644),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
- 
-  map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
-
-
-
-
-
-
-        var myLatlng = new google.maps.LatLng('photo.latitude','photo.longitude');
+      function init() {
         var mapOptions = {
-        zoom: 11,
-        center: myLatlng,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-        }
-        // instantiate a marker object
-        var marker = new google.maps.Marker({
-        position: myLatlng,
-        map: map,
-        // alt text - 
-        title:"ALT TEXT"
-  });
+          center: new google.maps.LatLng(37.463, 122.255),
+          zoom: 2,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+        {% for photo in photos %}
+
+          var content{{photo.id}} = '<div><IMG BORDER="0" ALIGN="Left" SRC="{{photo.thumbnail}}"><p>{{photo.caption}}</p></div>';
+          var marker{{photo.id}} = new google.maps.Marker({
+            position: new google.maps.LatLng({{photo.latitude}}, {{photo.longitude}}),
+            map: map,
+          });
+      
+          var infoWindow{{photo.id}} = new google.maps.InfoWindow({content: content{{photo.id}}});
+          google.maps.event.addListener(marker{{photo.id}}, 'click', 
+            makeInfoWindowEvent(map, infoWindow{{photo.id}}, marker{{photo.id}}));
 
 
-        //caption
-        var content = 'photo.caption';
-        //instantiating a new infowindow for the marker
-        var infoWindow = new google.maps.InfoWindow({content: content});
 
-        google.maps.event.addListener(marker, 'click', function() {
-            infoWindow.open(map, marker);
-            });
+        {% endfor %}
+
       }
-      google.maps.event.addDomListener(window, 'load', initialize);
+
+      google.maps.event.addDomListener(window, 'load', init);
+    </script>
