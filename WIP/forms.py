83d1a979a6801bd5af9af8ee0f4e5554e69e8bd0
Lauren <lauren@transmogrifier.(none)>
2013-05-04 23:00:41 -0700

    added map markerCluster

---
+++ WIP/forms.py
@@ -0,0 +1,11 @@
+# from flask.ext.wtf import Form
+# from wtforms import TextField, IntegerField, validators as v
+
+# class AddUserForm(Form):
+# 	email = TextField("email", validators=[v.required()])
+# 	username = TextField("username", validators=[v.required()])
+# 	password = TextField("password", validators=[v.required()])
+
+# class LoginUserForm(Form):
+# 	email = TextField("email", validators=[v.required()])
+# 	password = TextField("password", validators=[v.required()])
